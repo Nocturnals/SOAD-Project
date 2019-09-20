3067241824a6a9724f26a3526de6780084077a17
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-09-20 20:44:06 +0530

    Added joi validation

--- Backend/src/routes/auth.ts
+++ Backend/src/routes/auth.ts
@@ -1,15 +1,25 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
 import { Router } from 'express-serve-static-core';
 
+import Joi from '@hapi/joi';
+
 // const User = require('../models/user');
 import { UserModel } from '../models/user';
-import  mongoose  from 'mongoose';
 
 
 const router: Router = express.Router();
 
+const schema = {
+    name: Joi.string().min(4).max(255).required(),
+    email: Joi.string().min(5).max(255).required(),
+    password: Joi.string().min(8).max(1024).required()
+}
+
 // route to register new user
 router.post('/register', async (req: Request, res: Response) => {
+
+    const validate = Joi.validate(req.body, schema);
+    
     const user: any = new UserModel({
         name: req.body.name,
         email: req.body.email,
