3270c227680ed4a67c322ff6d29810fbbc7d9657
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-09-30 23:18:32 +0530

    Installed new packages

--- Backend/server.js
+++ Backend/server.js
@@ -24,7 +24,13 @@ const app = express();
 
 // Middleware
 app.use(express.json());        // for converting the json part of the request body
-app.use(morgan('tiny'));      // for logging the infomation
+app.use(express.static('public'));
+
+if(process.env.Node_Env === 'development') {
+    app.use(morgan('tiny'));      // for logging the infomation
+}
+
+
 if (process.env.Node_Env === 'development') {
     app.use(helmet());      // for securing the routes with adding headers
     console.log('Logging the data using morgan');
