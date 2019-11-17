df5c16dd4c970664b609899c2b120bb5e7efda39
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-11-17 08:59:45 +0530

    Removed errors

--- Backend/routes/subscription_1/service.js
+++ Backend/routes/subscription_1/service.js
@@ -6,8 +6,7 @@ postModel = require("./../../models/Post");
 planpostModel = require("./../../models/Subcriptions");
 const UserModel = require("./../../models/user");
 const { OtheruserModel } = require("./../../models/Otheruser");
-const {addtoplanValidation} = require("./serviceValidation")
-
+const { addtoplanValidation } = require("./serviceValidation");
 
 router.post(
     "/showallplans",
@@ -86,7 +85,6 @@ router.post(
     verifyToken,
     verifyUserWithToken,
     async (req, res, next) => {
-
         const validatedData = addtoplanValidation(req.body);
         if (validatedData.error)
             return res
@@ -174,15 +172,13 @@ router.post(
             const plan = await planModel.findById(req.params.id);
 
             if (plan) {
-            
-                var posts = []
-                plan.posts.forEach(i => {
-                    const post = await postModel.findById(i._id)
-                    if(post.category == req.params.category){
-                    posts.push(post);
-                 }
-                })
-
+                var posts = [];
+                plan.posts.forEach(async i => {
+                    const post = await postModel.findById(i._id);
+                    if (post.category == req.params.category) {
+                        posts.push(post);
+                    }
+                });
 
                 return res.send(posts);
             } else {
